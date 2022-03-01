6bebd018fdcfddadff99b479a832b84dfda18c8d
YOUNGWON1 <hopan678@gmail.com>
2022-03-01 23:40:27 +0900

    node

--- test.js
+++ test.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const Mongoose = require('mongoose');
 
 const app = express()
-
-app.listen(8080, function() {
-    console.log('server start on 8080')
-});
 
 app.get('/test', async(req, res) => {
     const v6 = 'sssssssss';
     res.status(200).send({v6});
   });
+
+const uri = "mongodb+srv://iium:wonder20@modioz.jck0n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+async function connectDB() {
+  return Mongoose.connect(uri)
+  .then(() => console.log('Connected'));
+};
+
+connectDB().then(() => {
+    console.log(`Server is started… ${new Date()}`);
+    app.listen(8080);
+  })
