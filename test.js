c31472b6034b49326fc35745c6ee334e96810e33
KKKYW <hopan678@gmail.com>
2022-03-02 09:10:57 +0900

    test

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ const Mongoose = require('mongoose');
 const app = express()
 
 app.get('/test', async(req, res) => {
-    const v6 = 'sssssssss';
-    res.status(200).send({v6});
+    const v123 = 'sssssssss';
+    res.status(200).send({v123});
   });
 
 const uri = "mongodb+srv://iium:wonder20@modioz.jck0n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
