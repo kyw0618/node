4167f4e4d52452c885f923c3b519f8317f8b8c75
KKKYW <hopan678@gmail.com>
2022-03-02 11:09:04 +0900

    GET 테스트

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
-const express = require('express')
+const express = require('express');
+const req = require('express/lib/request');
 const Mongoose = require('mongoose');
 
 const app = express()
 
 app.get('/test', async(req, res) => {
     const v123 = 'sssssssss';
+    res.status(200).send({v123});
+  });
+
+  app.get('/v1/app/policy', async(res, req) => {
+    const policy = await policy.find({});
     res.status(200).send({v123});
   });
 
@@ -19,3 +25,4 @@ connectDB().then(() => {
     console.log(`Server is started… ${new Date()}`);
     app.listen(8080);
   })
+
