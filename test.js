6701c7b5749fbd9c2be6766705580e5d80f48f3a
KKKYW <hopan678@gmail.com>
2022-03-03 10:55:25 +0900

    s

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const req = require('express/lib/request');
 const Mongoose = require('mongoose');
-const Account = require("./schema/accountModel");
 
 const app = express()
 
