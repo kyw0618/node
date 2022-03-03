53c96884813e1a8b7c0296aa57ee5cc7d891dad4
KKKYW <hopan678@gmail.com>
2022-03-03 17:51:32 +0900

    zzz

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import { config } from './config.js';
+import { connectDB } from './db/db.js';
 import appRouter from './router/app.js';
 import userRouter from './router/auth.js';
 
@@ -16,9 +17,14 @@ app.use('/v1/user', userRouter);
 
 app.use((req, res, next) => {
   res.sendStatus(404);
-})
+});
 
 app.use((error, req, res, next) => {
   console.log(error);
   res.sendStatus(500);
+});
+
+connectDB().then(() => {
+  console.log(`Server is started... ${new Date()}`);
+  app.listen(config.port);
 });
