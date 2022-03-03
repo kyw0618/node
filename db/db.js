da873abf3363d9b136dd8bb097c35a62ca748897
KKKYW <hopan678@gmail.com>
2022-03-03 17:56:54 +0900

    ss

--- db/db.js
+++ db/db.js
@@ -3,14 +3,17 @@ import admin from 'firebase-admin';
 import { config } from '../config.js';
 import { readFile } from 'fs/promises';
 
-const uri = "mongodb+srv://iium:wonder20@modioz.jck0n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const serviceAccount = JSON.parse(
+  await readFile(
+    new URL('../heaven-289ab-firebase-adminsdk-j7ifw-be12826516.json', import.meta.url)
+  )
+);
 
-async function connectDB() {
-  return Mongoose.connect(uri)
-  .then(() => console.log('Connected'));
-};
-
-connectDB().then(() => {
-    console.log(`Server is started… ${new Date()}`);
-    app.listen(8080);
-  })
+export async function connectDB() {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://heaven-289ab-default-rtdb.firebaseio.com"
+  });
+  return Mongoose.connect(config.db.host)
+    .then(() => console.log('MongoDB Connected...'));
+}
