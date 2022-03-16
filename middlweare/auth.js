d46305e46f26907365d6304db7b72e3585a8201b
KKKYW <hopan678@gmail.com>
2022-03-16 09:52:51 +0900

    test

--- middlweare/auth.js
+++ middlweare/auth.js
@@ -2,7 +2,6 @@ import jwt from 'jsonwebtoken';
 import { config } from '../config.js';
 import * as userRepository from '../data/auth.js';
 
-// 로그인 상태 확인
 export const isAuth = async (req, res, next) => {
   const token = req.get('Accesstoken');
   if(!(token))
