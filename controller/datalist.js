d546b2096ffd5c78e3b722786add4d8091657ab6
KKKYW <hopan678@gmail.com>
2022-04-22 17:34:34 +0900

    .

--- controller/datalist.js
+++ controller/datalist.js
@@ -2,7 +2,7 @@ import * as obitRepository from '../data/datalist.js';
 import { config } from '../config.js';
 
 
-export async function createObituary(req, res) {
+export async function createObituary(req, res) { 
   const imgName = res.req.file.filename;
   const {title, keyword, detail, timestamp} = req.body;
   const userId = req.userId;
