0e43ecbcaf1265950698f44fa4ebdc9b71fd0101
KKKYW <hopan678@gmail.com>
2022-05-09 17:38:39 +0900

    t

--- middlweare/textImg.js
+++ middlweare/textImg.js
@@ -1,12 +1,13 @@
 import multer from "multer";
 import fs from "fs";
+import path from "path";
 
 var storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploadsText/");
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}_${file.originalname}`);
+    cb(null, `${path.now()}_${file.originalname}`);
   },
  });
 
