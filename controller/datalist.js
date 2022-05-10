3fe5d29cd5393166bbbb8a342ef55dc8240d24d1
KKKYW <hopan678@gmail.com>
2022-05-10 17:58:12 +0900

    사진 배열로 업로드 완료 / 몽고DB에는 object로 저장되어지지 않음

--- controller/datalist.js
+++ controller/datalist.js
@@ -3,9 +3,7 @@ import { config } from '../config.js';
 
 
 export async function createObituary(req, res) { 
-  const imgName = {
-    imgName: res.req.files.filename
-  }
+  const imgName = res.req.files.filename
   const {title, keyword, detail, timestamp} = req.body;
   const userId = req.userId;
   
