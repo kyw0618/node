4f4d9b732fc5c8788228cce319d55a146a18574b
KKKYW <hopan678@gmail.com>
2022-05-18 17:33:49 +0900

    ㅅ

--- controller/datalist.js
+++ controller/datalist.js
@@ -129,10 +129,10 @@ export async function removeObit(req, res, next) {
 //데이터 조회
 export async function getMyObituary(req, res) {
   const textImg = await obitRepository.findMyObituary(req.userId);
-  const normalImg = await norimgRepository.findMyObituary(req.userId);
-  const video = await videoRepository.findMyObituary(req.userId);
+  // const normalImg = await norimgRepository.findMyObituary(req.userId);
+  // const video = await videoRepository.findMyObituary(req.userId);
 
-  res.status(200).json({"status": "200", textImg,normalImg,video});
+  res.status(200).json({"status": "200", textImg});
 }
 
 
