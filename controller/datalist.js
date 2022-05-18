3f4c05fe7a6ab5ab1907a3d01b340b92c0e32086
KKKYW <hopan678@gmail.com>
2022-05-18 13:23:54 +0900

    tt

--- controller/datalist.js
+++ controller/datalist.js
@@ -4,7 +4,7 @@ import * as videoRepository from '../data/video.js';
 import { config } from '../config.js';
 
 export async function createObituary(req, res) { 
-  const TextImg = req.files;
+  const TextImg = req.files.fileInfo.filepath;
   const {title, keyword,timestamp} = req.body;
   const userId = req.userId;
   
@@ -15,7 +15,7 @@ export async function createObituary(req, res) {
     timestamp,
     userId
   });
-  res.status(201).json({"status": "201", textImg, fileInfo : req.files.fileInfo});
+  res.status(201).json({"status": "201", textImg, fileInfo : req.files});
 } 
 
 export async function createNormalImg(req, res) { 
