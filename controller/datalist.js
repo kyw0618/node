962ebb41401808c9cd347e0292fcad036e961543
KKKYW <hopan678@gmail.com>
2022-05-19 15:08:30 +0900

    t

--- controller/datalist.js
+++ controller/datalist.js
@@ -58,7 +58,7 @@ export async function getTextImageData(req, res) {
   } catch {
     return res.status(404).json({"status": "404"}); 
   }
-  console.log(res.query.textimg)
+  console.log(req.query.textimgs)
   res.sendFile(filepath); 
 }
 
