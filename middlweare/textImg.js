98a6a79e837cde04c2337d3bf25e2079892c0968
KKKYW <hopan678@gmail.com>
2022-05-19 10:01:39 +0900

    t

--- middlweare/textImg.js
+++ middlweare/textImg.js
@@ -10,8 +10,6 @@ var storage = multer.diskStorage({
   },
  });
 
-// export var textImg = multer({ storage: storage}).array("textimg",5);
-export var textImg = multer({ storage: storage}).fields([{name:'text1'},
-{name: 'text2'}, {name: 'text3'},{name:'text4'},{name: 'text5'}],5);
+export var textImg = multer({ storage: storage}).array("textimg",5);
 
 
