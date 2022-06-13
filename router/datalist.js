b90ecb1c06bed577ad4a22cf9cef23a64761241f
KKKYW <hopan678@gmail.com>
2022-06-13 18:47:44 +0900

    사진 업로드

--- router/datalist.js
+++ router/datalist.js
@@ -3,13 +3,10 @@ import * as obitController from '../controller/datalist.js';
 import { isAuth } from '../middlweare/auth.js';
 import {textImg} from '../middlweare/datalist.js';
 
-
 const router = express.Router();
 
 router.post('/datalist', isAuth, textImg, obitController.createObituary);
-
 router.put('/:id', isAuth, obitController.updateObit);
-
 router.delete('/:id', isAuth, obitController.removeObit);
 
 router.get('/', isAuth, obitController.getByname);
