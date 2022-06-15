7f78b9be8d1aeb42cdb7518b9b175f3a72bcbc53
KKKYW <hopan678@gmail.com>
2022-06-15 13:20:17 +0900

    ㅅ

--- router/datasend.js
+++ router/datasend.js
@@ -7,9 +7,6 @@ const router = express.Router();
 //데이터 등록
 router.post('/datasend', isAuth, obitController.createObituary);
 
-//데이터 수정
-router.put('/', isAuth, obitController.updateObit);
-
 //데이터 삭제
 router.delete('/', isAuth, obitController.removeObit);
 
@@ -18,9 +15,6 @@ router.get('/', isAuth, obitController.getByname);
 
 //데이터 조회
 router.get('/my', isAuth, obitController.getMyObituary);
-
-//이미지 다운로드
-router.get('/datasend', obitController.getTextImageData);
 
 router.get('/:id', isAuth, obitController.getOneObituary);
 
