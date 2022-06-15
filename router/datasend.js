27b0f83c0242fa209bb85a7ce4f980b2bc197413
KKKYW <hopan678@gmail.com>
2022-06-15 14:09:27 +0900

    ㅅ

--- router/datasend.js
+++ router/datasend.js
@@ -5,7 +5,7 @@ import { isAuth } from '../middlweare/auth.js';
 const router = express.Router();
 
 //판매 데이터 등록
-router.post('/', isAuth, obitController.createObituary);
+router.post('/datasend', isAuth, obitController.createObituary);
 
 //판매 데이터 삭제
 router.delete('/', isAuth, obitController.removeObit);
