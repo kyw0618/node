1e28f4646cd25911753fd8646cbdd7d0ff9fd874
KKKYW <hopan678@gmail.com>
2022-06-22 09:09:00 +0900

    t

--- controller/dataocr.js
+++ controller/dataocr.js
@@ -1,17 +1,17 @@
 import * as obitRepository from '../data/dataocr.js';
 
 export async function createObituary(req, res) { 
-    // const {version,requestId,timestamp, images} = req.body.json;
-    // const userId = req.userId;
+    const {version,requestId,timestamp, images} = req.body.json;
+    const userId = req.userId;
   
-    // const dataocr = await obitRepository.save({
-    // version,
-    // requestId,
-    // timestamp, 
-    // images,
-    // userId
-    // });
-    res.status(201).json({"status": "201"});
+    const dataocr = await obitRepository.save({
+    version,
+    requestId,
+    timestamp, 
+    images,
+    userId
+    });
+    res.status(201).json({"status": "201",dataocr});
     console.log(req.body);
-  
+
   } 
