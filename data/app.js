e18f30a8e34ca6a5204e9ade715e4b6ebf2e37a6
KKKYW <hopan678@gmail.com>
2022-07-05 16:14:10 +0900

    ㅅ

--- data/app.js
+++ data/app.js
@@ -29,7 +29,7 @@ const policydb = Mongoose.model('Policy', policySchema, 'policy');
 const Verification = Mongoose.model('Verification', verification);
 const signupTerms = Mongoose.model('Terms', termsSchema, 'terms');
 
-export async function showPlicy() {
+export async function showPolicy() {
   return policydb.findOne();
 }
 
