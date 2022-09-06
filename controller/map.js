e79976ffc6ec2ea924fb541d6f9152af953ba1f1
KKKYW <hopan678@gmail.com>
2022-09-06 10:09:34 +0900

    2022-09-06

--- controller/map.js
+++ controller/map.js
@@ -1,79 +1,80 @@
-import * as obitRepository from '../data/map.js';
+import * as mapRepository from '../data/map.js';
 
+export async function createNap(req, res) {
+  const address = {
+    address_name,
+    region_1depth_name,
+    region_2depth_name,
+    region_3depth_name,
+    call,
+    x,
+    y
+  };
+  const {created} = req.body;
 
-export async function mapfun(req, res) {
-    const mapData = await naverMap.showMap();
-    res.status(200).json(mapData);
-  }
-
-  export async function createMap(req, res) { 
-    const {result,map} = req.body;
-  
-    const naverMap = await obitRepository.save({
-      result,
-      map
-    });
-    res.status(201).json({"status": "201", naverMap});
-  } 
-
-  export async function updateObit(req, res, next) {
-    const id = req.query.id;
-    const {name,xvalue,yvalue,address,call } = req.body;
-  
-    const obit = await obitRepository.findById(id);
-    if(!obit) {
-      return res.status(404).json({"status":"404"});
-    }
-    if(obit.userId !== req.userId && config.adminId !== req.userId) {
-      return res.status(403).json({"status": "403"});
-    }
-  
-    const updatedObit = await obitRepository.update(
-      name,
-      xvalue,
-      yvalue,
+  const userId = req.userId;
+  try {
+    var map_address = await mapRepository.save({
       address,
-      call
-      );
-    res.status(200).json({"status": "200", updatedObit});
+      created,
+      userId
+    });
+  } catch (error) {
+    return res.status(400).json({"status" : "400"});
   }
-
-  export async function removeObit(req, res, next) {
-    const id = req.query.id; 
-    const obit = await obitRepository.findById(id);
-    if(!obit) {
-      return res.status(404).json({"status":"404"});
-    }
-    if(obit.userId !== req.userId && config.adminId !== req.userId) {
-      return res.status(403).json({"status": "403"});  
-    }
   
-    await obitRepository.remove(id);
-    res.status(204).json(({"status":"204"}))
+  res.status(201).json({"status": "201", map_address});
 }
 
-//데이터 조회
-export async function getMyObituary(req, res) {
-    const naverMap = await obitRepository.findMyObituary(req.userId);
-    res.status(200).json({"status": "200", naverMap});
+export async function updateAddress(req, res, next) {
+  const id = req.params.id;
+  const obit = await mapRepository.findById(id);
+  
+  if(!obit) {
+    return res.status(404).json({"status":"404"});
   }
-  
-  
-  export async function getOneObituary(req, res) {
-    const obId = req.query.id;
-  
-    const obit = await obitRepository.findById(obId);
-  
-    res.status(200).json({"status": "200", obit});
-  
+  if(obit.userId !== req.userId && req.admin == false) {
+    return res.status(403).json({"status": "403"});
   }
-  
-  //데이터 검색
-  export async function getByname(req, res) {
-    const value = req.query.name;
-    const result = await ( value 
-      ? obitRepository.findObituaryByname(value)
-      : obitRepository.getAllObituary());
-    
-    res.status(200).json({"status": "200", result});
+
+  const address = {
+    address_name,
+    region_1depth_name,
+    region_2depth_name,
+    region_3depth_name,
+    call,
+    x,
+    y
+  };
+  const {created} = req.body;
+
+  const updateAddress = await mapRepository.update(
+    id,
+    address,
+    created,
+  );
+  res.status(200).json({"status": "200", updateAddress});
+}
+
+export async function removeAddress(req, res, next) {
+  const id = req.params.id;
+  const obit = await mapRepository.findById(id);
+  if(!obit) {
+    return res.status(404).json({"status":"404"});
   }
+  if(obit.userId !== req.userId && req.admin == false) {
+    return res.status(403).json({"status": "403"});
+  }
+
+  await mapRepository.remove(id);
+  res.status(204).json(({"status":"204"}))
+}
+
+export async function getByname(req, res) {
+  const value = req.query.name;
+  const result = await ( value 
+    ? mapRepository.findObituaryByname(value)
+    : mapRepository.getAllObituary());
+  
+  res.status(200).json({"status": "200", result});
+}
--- data/map.js
+++ data/map.js
@@ -2,62 +2,43 @@ import Mongoose from 'mongoose';
 import {useVirtualId} from '../db/db.js';
 
 const mapSchema = new Mongoose.Schema( {
-  result: {type: Boolean},
-  map: {type: Array},
+  address: {type: Object},
+  created: {type: String},
+  userId: {type: String}
   }, {
     versionKey: false
-  });
+});
 
-  useVirtualId(mapSchema);
+useVirtualId(mapSchema);
 
-  const mapdb = Mongoose.model('Navermap', mapSchema, 'navermap');
-
-export async function showMap() {
-    return mapdb.findOne();
-}
+const MapSchema = Mongoose.model('MapSchema', mapSchema);
 
 export async function getAllObituary() {
-  return mapdb.find().sort({ createdAt: -1});
- }
-  
-export async function findById(id) {
-  return mapdb.findById(id);
- }
-  
-export async function findMyObituary(userId) {
-  return mapdb.find({userId}).sort({ createdAt: -1});
- }
-  
-export async function findObituaryByname(name) {
-  return mapdb.find({ $or: [{"title": name},
-      {"keyword": name}]}).sort({ createdAt: -1});
-}
-  
-export async function save(obit) {
-  return new mapdb(obit).save()
-    .then((data) => data);
+  return MapSchema.find().sort({ createdAt: -1});
 }
 
-export async function update( 
-  name,
-  xvalue,
-  yvalue,
-  address,
-  call
-  ) {
-  return DataList.findByIdAndUpdate(
-    id, {
-      name,
-      xvalue,
-      yvalue,
-      address,
-      call
-    }, 
-    {
-      returnOriginal: false}
-      );
+export async function findById(id) {
+  return MapSchema.findById(id);
+}
+
+export async function findMyObituary(userId) {
+  return MapSchema.find({userId}).sort({ createdAt: -1});
+}
+
+export async function findObituaryByname(name) {
+  return MapSchema.find({ $or: [{"resident.name": name},
+      {"deceased.name": name}, {"place": name}]}).sort({ createdAt: -1});
+}
+
+export async function save(obit) {
+  return new MapSchema(obit).save()
+  .then((data) => data);
+}
+
+export async function update(id, resident, place, deceased, eod, coffin, dofp, buried, word) {
+  return MapSchema.findByIdAndUpdate(id, {resident, place, deceased, eod, coffin, dofp, buried, word}, {returnOriginal: false});
 }
 
 export async function remove(id) {
-  return DataList.findByIdAndDelete(id);
+  return MapSchema.findByIdAndDelete(id);
 }
