e2fcd159803d0533ba7af3dd07fc175cc1d51379
KKKYW <hopan678@gmail.com>
2022-09-14 13:15:21 +0900

    t

--- controller/map.js
+++ controller/map.js
@@ -86,7 +86,8 @@ export async function getByname(req, res) {
   const result = await ( value 
     ? mapRepository.findObituaryByname(value)
     : mapRepository.getAllObituary(),
-    mapRepository.findPlaceyByname(value)
+    mapRepository.findPlaceyByname(value),
+    mapRepository.findRoadByname(value),
     );
   res.status(200).json({"status": "200", result});
 }
