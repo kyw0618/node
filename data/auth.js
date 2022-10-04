5a111f5c8db973edec6a8e648fabb392cf415f6b
KKKYW <hopan678@gmail.com>
2022-10-04 13:03:26 +0900

    2022-10-04

--- data/auth.js
+++ data/auth.js
@@ -2,7 +2,7 @@ import Mongoose from 'mongoose';
 import {useVirtualId} from '../db/db.js';
 
 const users = new Mongoose.Schema( {
-  imgName: {type: String, requirer: true},
+  imgName: {type: String},
   phone: {type: String, requirer: true},
   sex: {type: String, requirer: true},
   name: {type: String, requirer: true},
